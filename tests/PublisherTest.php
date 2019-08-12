16c5722d2101f93a88eaf6e694772ecc81f48067
emre can islambey <eislambey@gmail.com>
2019-08-12 09:27:46 +0300

    Fix publish test

--- tests/PublisherTest.php
+++ tests/PublisherTest.php
@@ -8,25 +8,34 @@ use Symfony\Component\Mercure\Update;
 use Amp\Success;
 use Islambey\Amp\Mercure\Publisher;
 use function Amp\Promise\wait;
+use Amp\Artax\Request;
 
 class PublisherTest extends TestCase
 {
     public function testPublish()
     {
+        \DG\BypassFinals::enable();
         $jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJmb28iLCJiYXIiXSwicHVibGlzaCI6WyJmb28iXX19.LRLvirgONK13JgacQ_VbcjySbVhkSmHy3IznH3tA9PM';
+        $hubUri = 'https://example.com/hub';
         $jwtProvider = new StaticJwtProvider($jwt);
         $update = new Update('https://example.com', 'data');
-        $success = new Success('id');
+        $message = new \Amp\ByteStream\Message(new \Amp\ByteStream\InMemoryStream('id'));
 
-        $publisher = $this->getMockBuilder(Publisher::class)
-            ->setConstructorArgs(['https://example.com', $jwtProvider])
+        $httpClient = $this->getMockBuilder(\Amp\Artax\DefaultClient::class)
             ->getMock();
 
-        $publisher->expects($this->once())
-            ->method('publish')
-            ->with($update)
-            ->willReturn($success);
+        $response = $this->getMockBuilder(\Amp\Artax\Response::class)
+            ->getMock();
 
+        $response->expects($this->once())
+            ->method('getBody')
+            ->willReturn($message);
+        $httpClient->expects($this->once())
+            ->method('request')
+            ->with($this->isInstanceOf(Request::class))
+            ->willReturn(new Success($response));
+
+        $publisher = new Publisher($hubUri, $jwtProvider, $httpClient);
         $value = wait($publisher->publish($update));
 
         $this->assertSame('id', $value);
