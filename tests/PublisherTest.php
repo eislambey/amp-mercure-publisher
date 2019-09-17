2da69fefe65f9a3f1bc3bcb8d83c15cb34a6a9f6
eislambey <eislambey@gmail.com>
2019-09-17 07:43:03 +0300

    Update tests for better code readability

--- tests/PublisherTest.php
+++ tests/PublisherTest.php
@@ -2,33 +2,38 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Amp\Artax\DefaultClient;
+use Amp\Artax\Response;
 use Amp\PHPUnit\AsyncTestCase;
+use DG\BypassFinals;
 use Symfony\Component\Mercure\Jwt\StaticJwtProvider;
 use Symfony\Component\Mercure\Update;
 use Amp\Success;
 use Islambey\Amp\Mercure\Publisher;
 use Amp\Artax\Request;
+use Amp\ByteStream;
 
 class PublisherTest extends AsyncTestCase
 {
     public function testPublish()
     {
-        \DG\BypassFinals::enable();
+        BypassFinals::enable();
         $jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJmb28iLCJiYXIiXSwicHVibGlzaCI6WyJmb28iXX19.LRLvirgONK13JgacQ_VbcjySbVhkSmHy3IznH3tA9PM';
         $hubUri = 'https://example.com/hub';
         $jwtProvider = new StaticJwtProvider($jwt);
         $update = new Update('https://example.com', 'data');
-        $message = new \Amp\ByteStream\Message(new \Amp\ByteStream\InMemoryStream('id'));
+        $message = new ByteStream\Message(new ByteStream\InMemoryStream('id'));
 
-        $httpClient = $this->getMockBuilder(\Amp\Artax\DefaultClient::class)
+        $httpClient = $this->getMockBuilder(DefaultClient::class)
             ->getMock();
 
-        $response = $this->getMockBuilder(\Amp\Artax\Response::class)
+        $response = $this->getMockBuilder(Response::class)
             ->getMock();
 
         $response->expects($this->once())
             ->method('getBody')
             ->willReturn($message);
+
         $httpClient->expects($this->once())
             ->method('request')
             ->with($this->isInstanceOf(Request::class))
